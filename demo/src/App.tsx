a06d02d7dbd01fbe7cb4733de29e5f2cf150541f
Yuqian Chen <u7781990@anu.edu.au>
2026-08-05 16:05:14 +1000

    first commit

---
+++ demo/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+// import CardList from "./components/CardList";
+// import Search from "./components/Search/Search";
+function App() {
+  //   const [search, setSearch] = useState<string>("");
+
+  //   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //     setSearch(e.target.value);
+  //     console.log(e);
+  //   };
+
+  //   const onClick = (e: SyntheticEvent) => {
+  //     console.log(e);
+  //   };
+  return (
+    <main>
+      <h1>React App</h1>
+      <p>项目已成功运行。</p>
+    </main>
+  )
+}
+
+export default App
